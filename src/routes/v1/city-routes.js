23cff181d1102ff02cf7c5bb6cc07dc3a82a4b0d
raunak-alobha <raunak.alobha@gmail.com>
2025-02-25 00:22:37 +0530

    Added middlewares for the city

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { CityController } = require('../../controllers/index');
+const { CityMiddlewares } = require('../../middlewares/index');
 
 //router object
 const router = express.Router();
 
 //create a city
-router.post('/create-city', CityController.createCity);
+router.post('/create-city', CityMiddlewares.validateCreateRequest, CityController.createCity);
 
 
 //export the router
